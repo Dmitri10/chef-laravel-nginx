15031d4b1fd9dc2a72be23a549848f18f9441832
Mathias Hansen <me@codemonkey.io>
2014-03-08 16:40:21 -0500

    Updated resource with new SSL attributes

--- resources/site.rb
+++ resources/site.rb
@@ -10,4 +10,6 @@ default_action :create
 attribute :host, :kind_of => String, :default => 'localhost'
 attribute :root, :kind_of => String, :default => nil
 attribute :code, :kind_of => String, :default => nil
+attribute :ssl_certificate, :kind_of => String, :default => nil
+attribute :ssl_certificate_key, :kind_of => String, :default => nil
 
