988642de019ac04c4a1b7fe5c8530dc8e243279d
Mathias Hansen <me@codemonkey.io>
2014-06-12 16:50:47 -0400

    Bumped version number

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email 'me@codemonkey.io'
 license          'MIT'
 description      'Installs/Configures Nginx sites for Laravel with PHP-FPM'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.3.6'
+version          '0.3.7'
 
 depends          'nginx'
 depends          'php-fpm'
